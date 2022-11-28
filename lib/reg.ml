aac17815c65cf0fe97b662c8ac0a0e82eaed6ad6
Dimitrije Radojević <me@dimitrije.website>
2022-11-28 22:15:44 +0100

    Fix Reg module.

--- lib/reg.ml
+++ lib/reg.ml
@@ -11,7 +11,7 @@ end
 
 module Make (A : Data.ADDR) (D : Data.DATA) = struct
   type data = D.t
-  type reg = A.reg
+  type r = A.reg
 
   type t =
     { r0 : D.t
@@ -36,40 +36,40 @@ module Make (A : Data.ADDR) (D : Data.DATA) = struct
     }
   ;;
 
-  let to_string r =
+  let to_string reg =
     Printf.sprintf
       "[%d %d %d %d %d %d %d %d]"
-      (D.to_int r.r0)
-      (D.to_int r.r1)
-      (D.to_int r.r2)
-      (D.to_int r.r3)
-      (D.to_int r.r4)
-      (D.to_int r.r5)
-      (D.to_int r.r6)
-      (D.to_int r.r7)
+      (D.to_int reg.r0)
+      (D.to_int reg.r1)
+      (D.to_int reg.r2)
+      (D.to_int reg.r3)
+      (D.to_int reg.r4)
+      (D.to_int reg.r5)
+      (D.to_int reg.r6)
+      (D.to_int reg.r7)
   ;;
 
-  let read i r =
-    match i with
-    | Data.A.R0 -> r.r0
-    | Data.A.R1 -> r.r1
-    | Data.A.R2 -> r.r2
-    | Data.A.R3 -> r.r3
-    | Data.A.R4 -> r.r4
-    | Data.A.R5 -> r.r5
-    | Data.A.R6 -> r.r6
-    | Data.A.R7 -> r.r7
+  let read r reg =
+    match r with
+    | A.R0 -> reg.r0
+    | A.R1 -> reg.r1
+    | A.R2 -> reg.r2
+    | A.R3 -> reg.r3
+    | A.R4 -> reg.r4
+    | A.R5 -> reg.r5
+    | A.R6 -> reg.r6
+    | A.R7 -> reg.r7
   ;;
 
-  let write i v r =
-    match i with
-    | Data.A.R0 -> { r with r0 = v }
-    | Data.A.R1 -> { r with r1 = v }
-    | Data.A.R2 -> { r with r2 = v }
-    | Data.A.R3 -> { r with r3 = v }
-    | Data.A.R4 -> { r with r4 = v }
-    | Data.A.R5 -> { r with r5 = v }
-    | Data.A.R6 -> { r with r6 = v }
-    | Data.A.R7 -> { r with r7 = v }
+  let write r v reg =
+    match r with
+    | A.R0 -> { reg with r0 = v }
+    | A.R1 -> { reg with r1 = v }
+    | A.R2 -> { reg with r2 = v }
+    | A.R3 -> { reg with r3 = v }
+    | A.R4 -> { reg with r4 = v }
+    | A.R5 -> { reg with r5 = v }
+    | A.R6 -> { reg with r6 = v }
+    | A.R7 -> { reg with r7 = v }
   ;;
 end
