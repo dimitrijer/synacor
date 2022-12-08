b9e3ca726997a28ec1a082bbf42ef784b353591b
Dimitrije Radojević <me@dimitrije.website>
2022-12-08 10:03:50 +0100

    Use destructive substitution for data and addr.

--- lib/arch/arch.mli
+++ lib/arch/arch.mli
@@ -9,10 +9,10 @@ module A : Data.ADDR
 module RI : Data.ADDR with type t = Reg.r
 
 (** [M] is the memory module of Synacor arch. *)
-module M : Mem.MEM with type data = D.t with type addr = A.t
+module M : Mem.MEM with type data := D.t with type addr := A.t
 
 (** [R] is the register module of Synacor arch. *)
-module R : Reg.REG with type data = D.t with type addr = RI.t
+module R : Reg.REG with type data := D.t with type addr := RI.t
 
 (** [S] is the stack module of Synacor arch. *)
 module S : module type of Stack
